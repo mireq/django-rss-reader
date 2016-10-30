3eba4de038e10159d191d44725db8a1904921cd9
Miroslav Bendík <miroslav.bendik@gmail.com>
2016-10-30 20:29:04 +0100

    Using timezone instead of datetime

--- web/time_utils.py
+++ web/time_utils.py
@@ -4,15 +4,15 @@ from __future__ import unicode_literals
 from datetime import datetime as python_datetime
 from time import mktime
 
-from django.utils.timezone import utc
+from django.utils import timezone
 
 
 def timestamp_to_datetime(timestamp):
-	return python_datetime.fromtimestamp(timestamp, utc)
+	return python_datetime.fromtimestamp(timestamp, timezone.utc)
 
 
 def datetime_to_timestamp(datetime=None):
 	if datetime is None:
-		datetime = python_datetime.now(utc)
+		datetime = timezone.now()
 	timestamp = mktime(datetime.timetuple()) + (datetime.microsecond / 1000000.)
 	return timestamp
