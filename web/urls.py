461f798c6ed2a4f3dab6deb3acfddfecf769755c
Miroslav Bendík <miroslav.bendik@gmail.com>
2016-11-20 19:11:15 +0100

    Pridaný debug toolbar

--- web/urls.py
+++ web/urls.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
 
@@ -13,3 +14,9 @@ urlpatterns = [
 	url(r'^entry/(?P<pk>\d+)/$', feeds.views.entry_detail, name='entry_detail'),
 	url(r'^$', feeds.views.entry_list, name='entry_list'),
 ]
+
+if settings.DEBUG:
+	import debug_toolbar
+	urlpatterns += [
+		url(r'^__debug__/', include(debug_toolbar.urls)),
+	]
