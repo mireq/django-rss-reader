28faca4602b8e6171b71307baf9b3dcc3799bda0
Miroslav Bendík <miroslav.bendik@gmail.com>
2017-04-02 18:09:25 +0200

    Fixed timezones

--- web/time_utils.py
+++ web/time_utils.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import calendar
+import time
 from datetime import datetime as python_datetime
-from time import mktime
 
 from django.utils import timezone
 
 
 def timestamp_to_datetime(timestamp):
-	return python_datetime.fromtimestamp(timestamp).replace(tzinfo=timezone.utc)
+	return python_datetime(*time.gmtime(timestamp)[:6], tzinfo=timezone.utc)
 
 
 def datetime_to_timestamp(datetime=None):
 	if datetime is None:
 		datetime = timezone.now()
-	timestamp = mktime(datetime.timetuple()) + (datetime.microsecond / 1000000.)
+	timestamp = calendar.timegm(datetime.utctimetuple()) + (datetime.microsecond / 1000000.)
 	return timestamp
